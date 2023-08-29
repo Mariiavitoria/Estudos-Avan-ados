8d0111b12d38a32cf9d540fe8d18e7565c5a2802
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-08-29 08:20:15 -0300

    Add files via upload

---
+++ areacirculo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    float r,x;
+    printf("digite o raio:/n");
+    scanf ("%f", &r);
+    x=3.14*pow(r,2);
+    printf ("valor da area:%f" ,x);
+}
+
