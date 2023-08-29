7670a6694d87da1c1e80e62c6a04eda1ed011c5e
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-08-29 08:00:51 -0300

    Add files via upload

--- XeY.c
+++ XeY.c
@@ -3,5 +3,19 @@
 int main()
 {
    int x,y; //Esta frase é comentário
+
+printf ("digite x:/n");
+scanf("%i",&x);
+printf("digite y:/n");
+scanf ("%i",&y);
+
+if(x>y)
+    printf("x é maior/n");
+else{
+if(y>x){
+    printf("y é maior/n");
+   }else{
+    printf("x e y são iguais/n");
+   }
+ }
 }
-printf ('digite )
