5ecff4bbe241a50ab4eb3df024351771120ccc3e
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 20:47:55 -0300

    Add files via upload

---
+++ volume_esfera.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+   float main (){
+   float R,V;
+
+   printf("Digite R: ");
+   scanf("%f", &R);
+
+   V=(4/3)*3.14*(R*R*R);
+
+   printf("o volume da esfera e de: %3.f", V);
+
+   }
