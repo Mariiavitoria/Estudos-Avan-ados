3a6667169eadb4dce07484711757c40fbcddfb83
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 17:50:54 -0300

    Add files via upload

---
+++ area_circunferencia.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+   float main(){
+   float A,R;
+
+   printf("Digite R: ");
+   scanf("%f", &R);
+
+   A=3.14*(R*R);
+
+   printf("O valor da area da circunferencia e de: %f", A);
+
+   }
