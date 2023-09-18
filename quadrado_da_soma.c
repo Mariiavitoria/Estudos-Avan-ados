0718d6f59b76a61661773d01ba0548425531ca9a
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 16:57:15 -0300

    Add files via upload

---
+++ quadrado_da_soma.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   int main () {
+   int A,B,C,X;
+
+   printf("Digite A: ");
+   scanf("%i", &A);
+   printf("Digite B: ");
+   scanf("%i", &B);
+   printf("Digite C: ");
+   scanf("%i", &C);
+
+   X=(A+B+C)*(A+B+C);
+
+   printf("O quadrado da soma e de: %i", X);
+
+   }
