0a1a6d9ef377a3629134d370531e9a21c5c4802f
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 16:12:06 -0300

    Add files via upload

---
+++ DR.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+   float main() {
+   float A,B,C;
+
+   printf("Digite A: ");
+   scanf("%f", &A);
+   printf("Digite B: ");
+   scanf("%f", &B);
+
+   C=A*B;
+
+   printf("O valor em real e de: %f", C);
+
+   }
