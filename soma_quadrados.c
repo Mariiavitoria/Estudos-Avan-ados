f195e959474cdc17ea3ecb76a989e1d8a7b9d9c5
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 16:34:50 -0300

    Add files via upload

---
+++ soma_quadrados.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   int main () {
+   int A,B,C,X;
+
+   printf("Digite A: ");
+   scanf("%i", &A);
+   printf("Digite B: ");
+   scanf("%i",&B);
+   printf("Digite C: ");
+   scanf("%i", &C);
+
+   X=(A*A)+(B*B)+(C*C);
+
+   printf("A soma dos quadrados e de: %i", X);
+
+   }
