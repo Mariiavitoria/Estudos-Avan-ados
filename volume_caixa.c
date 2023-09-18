3d3f4f0e680e0c81d4334cc76a28d652e65d3376
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 15:38:50 -0300

    Add files via upload

---
+++ volume_caixa.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   float main () {
+   float V,C,L,H;
+
+   printf("Digite C: ");
+   scanf("%f", &C);
+   printf("Digite L: ");
+   scanf("%f", &L);
+   printf("Digite H: ");
+   scanf("%f", &H);
+
+   V=C*L*H;
+
+   printf("O volume da caixa e de: %f", V);
+
+   }
