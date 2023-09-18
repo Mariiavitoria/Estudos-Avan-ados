4e9388bbbf277674be391bc8e0f2ff782fec7a11
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 17:15:42 -0300

    Add files via upload

---
+++ produto_e_soma.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+   int main () {
+   int A,B,C,D,P,S;
+
+   printf("Digite A: ");
+   scanf("%i", &A);
+   printf("Digite B: ");
+   scanf("%i", &B);
+   printf("Digite C: ");
+   scanf("%i", &C);
+   printf("Digite D: ");
+   scanf("%i", &D);
+
+   P=A*C;
+   S=B+D;
+
+   printf("O produto de a e c e de: %i", P);
+
+   printf("A soma de b e d e de: %i", S);
+
+   }
