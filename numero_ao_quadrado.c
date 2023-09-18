30ff2e120427aa6c40ebd07a736918a4b8cad411
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 15:49:38 -0300

    Add files via upload

---
+++ numero_ao_quadrado.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+   int main () {
+
+   int N,X;
+
+   printf("Digite N: ");
+   scanf("%i", &N);
+
+   X=N*N;
+
+   printf("O valor do numero ao quadrado e de: %i", X);
+
+   }
