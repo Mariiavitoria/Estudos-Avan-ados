c8bcd85908cd0ada1ccbcfb1ef6f4731ad24cb78
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 15:57:49 -0300

    Add files via upload

---
+++ quadrado_da_diferenca.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+   int main (){
+   int A,B,C;
+
+   printf("Digite A: ");
+   scanf("%i", &A);
+   printf("Digite B: ");
+   scanf("%i", &B);
+
+   C=(A-B)*(A-B);
+
+   printf("O valor do quadrado da diferenca de a e b e de: %i", C);
+
+   }
