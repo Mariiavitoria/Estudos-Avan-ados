fae4faa58e20fced3c6ce3d1590b739953c642e3
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 21:47:45 -0300

    Add files via upload

---
+++ quadrado_da_divisao.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+   int main (){
+   int A,B,C;
+
+   printf("Digite A: ");
+   scanf("%i", &A);
+   printf("Digite B: ");
+   scanf("%i", &B);
+
+   C=(A/B)*(A/B);
+
+   printf("O quadrado da divisao e de: %i", C);
+
+   }
