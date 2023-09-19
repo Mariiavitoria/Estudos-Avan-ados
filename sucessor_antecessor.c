ea0e7f99b62adcca2df1a481d664b9e7579b249a
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 21:42:28 -0300

    Add files via upload

---
+++ sucessor_antecessor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+   int main (){
+   int N,A,S;
+
+   printf("Digite N: ");
+   scanf("%i", &N);
+
+   A=N-1;
+   S=N+1;
+
+   printf("O antecessor do numero e: %i \n", A);
+   printf("O sucessor do numero e: %i \n", S);
+
+   }
