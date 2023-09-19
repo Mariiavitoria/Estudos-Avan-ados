51bddfc09949dafa8617e55fe52bdc6233531109
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-09-18 21:03:26 -0300

    Add files via upload

---
+++ potencia.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+   int main () {
+   int B,E,P;
+
+   printf("Digite B: ");
+   scanf("%i", &B);
+   printf("Digite E: ");
+   scanf("%i", &E);
+
+   P= pow(B , E);
+
+   printf("O resultado da potencia e de: %i", P);
+
+   }
