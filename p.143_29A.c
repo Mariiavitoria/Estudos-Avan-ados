602498898f13cf43f0e0450c2693c34b77e476a3
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-10-02 20:15:01 -0300

    Add files via upload

---
+++ p.143_29A.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   int main(){
+   int I;
+
+   printf("Digite I: ");
+   scanf("%i", &I);
+
+   if(I>=18)
+   {
+     printf("Maior de idade");
+   }
+   else if(I<18)
+   {
+     printf("Menor de idade");
+   }
+   }
