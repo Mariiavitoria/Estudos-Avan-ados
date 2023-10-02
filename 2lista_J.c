a5d26c08fe32500f696548ab397ba33415ad13e9
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-10-02 19:19:30 -0300

    Add files via upload

---
+++ 2lista_J.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   int main(){
+   int N;
+
+   printf("Digite N: ");
+   scanf("%i", &N);
+
+   if(N%2==0)
+   {
+     printf("O numero e par");
+   }
+   else
+   {
+       printf("O numero e impar");
+   }
+   }
