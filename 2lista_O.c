f981977d4850d7a37c3b40cefa688e504ea6f0cc
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-10-02 20:05:31 -0300

    Add files via upload

---
+++ 2lista_O.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+   int main(){
+   int N,X;
+
+   printf("Digite N: ");
+   scanf("%i", &N);
+
+   X=N*2;
+
+   if(X>30)
+   {
+       printf("O valor da multiplicacao e maior que 30, e de: %i", X);
+   }
+
+   }
