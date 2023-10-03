3e14454e655754550c415dc7494336fe3c269f06
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-10-03 08:38:17 -0300

    Add files via upload

---
+++ p.143_35.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+   float main(){
+   float N;
+
+   printf("Digite N: \n");
+   scanf("%f", &N);
+
+   if(N>9)
+   {
+       printf("Conceito A");
+   }
+   else if(7<N&&N<=9)
+   {
+       printf("Conceito B");
+   }
+   else if(5<N&&N<=7)
+   {
+       printf("Conceito C");
+   }
+   else if(3,5<N&&N<=5)
+   {
+       printf("Conceito D");
+   }
+   else if(N<3.5)
+   {
+       printf("Conceito E");
+   }
+   }
