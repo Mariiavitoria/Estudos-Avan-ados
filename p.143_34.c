64cbfdf9f91e0e3bb783766d7016630da09a15d7
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-10-03 08:26:41 -0300

    Add files via upload

---
+++ p.143_34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+   float main(){
+   float N;
+
+   printf("Digite N: \n");
+   scanf("%f", &N);
+
+   if(N>3)
+   {
+       printf("O numero e maior que tres");
+   }
+   else if(N<=3)
+   {
+       printf("O numero e menor ou igual a tres");
+   }
+   }
