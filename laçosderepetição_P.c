4b50512b70e10724b14c7e55d8fefa1e9ae958db
Mariiavitoria <142894311+Mariiavitoria@users.noreply.github.com>
2023-11-23 17:28:44 -0300

    Add files via upload

---
+++ laçosderepetição_P.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+   int main(){
+   float B,C;
+
+   B=0;
+   for(C=50;C<=70;C++)
+   {
+       B=B+C;
+   }
+   printf("A soma e de: %.2f\n", B);
+   printf("A media e de: %.2f\n", B/20);
+
+   }
